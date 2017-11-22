be5c08a778e8c79533ef936790b00d1c12e48039
Oleg Gorecan <rvo@btinternet.com>
2017-11-22 16:41:47 +0000

    Add project files.

---
+++ CrossingRiverMonWk1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossingRiverMonWk1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The farmer takes the rabbit to the other side, leaving the fox with the carrots.\n");
+            Console.ReadKey();
+           
+            Console.WriteLine("The farmer leaves the rabbit on the other side and returns to the fox and the carrots.\n");
+            Console.ReadKey();
+
+            Console.WriteLine("Next, the farmer takes the carrots to the other side...\n");
+            Console.ReadKey();
+
+            Console.WriteLine("...but we he gets there, he leaves the carrots there and takes the rabbit back with him.\n ");
+            Console.ReadKey();
+
+            Console.WriteLine("Once he's back, he swaps the rabbit for the fox and takes the fox with him, leaving him on other side with the carrots.\n");
+            Console.ReadKey();
+
+
+            Console.WriteLine("He then finally goes back to get the rabbit.\n");
+            Console.ReadLine();
+
+
+        }
+    }
+}
